5afaecb2c3ded143f8a2851649938da5f0cc91a4
Andre Servidoni <deko.puc@gmail.com>
2020-10-24 23:25:38 +0200

    Initial Commit

---
+++ Assets/Scripts/v2/HitCheck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HitCheck : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			var enemyController = GetComponentInParent<Enemy2>();
+
+			if (!enemyController) {
+				Debug.LogError("Missing parent enemy controller!");
+				return;
+			}
+
+			enemyController.Hit(2);
+			
+		}
+	}
+}
---
+++ Assets/Scripts/v2/Patrol.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+	[SerializeField] private GameObject enemy = null;
+    [SerializeField] private GameObject[] myWaypoints; 
+	[SerializeField] private float waitAtWaypointTime = 1f;   
+	[SerializeField] private int myWaypointIndex = 0;
+
+	void Awake() {
+		if(!enemy)
+			Debug.LogError("Missing enemy!");
+	}
+
+    void Start() {
+        var newEnemy = Instantiate(enemy,
+                new Vector3(transform.position.x, transform.position.y, 0.0f),
+                Quaternion.identity);
+
+		newEnemy.transform.parent = transform;
+		newEnemy.GetComponent<Enemy2>()
+			.SetupPatrol(myWaypoints, waitAtWaypointTime, myWaypointIndex);
+    }
+}
